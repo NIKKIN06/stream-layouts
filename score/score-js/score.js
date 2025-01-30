677116bd39006fed406c33620a74593cc5797c31
Nikita Naumov <05.naumov.07.2006@gmail.com>
2025-01-30 23:54:17 +0200

    finished score layout | there is a working timer, there are chances to change in hud names, points and players of teams and the format of match thanks to json file

--- score/score-js/score.js
+++ score/score-js/score.js
@@ -1,4 +1,30 @@
+fetch('../../user-info/user-info.json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('left-team-name').textContent = data['ui-info']['ui-score']['teams'][0].name;
+    document.getElementById('right-team-name').textContent = data['ui-info']['ui-score']['teams'][1].name;
+
+    document.getElementById('left-team-points').textContent = data['ui-info']['ui-score']['teams'][0].points;
+    document.getElementById('right-team-points').textContent = data['ui-info']['ui-score']['teams'][1].points;
+
+    document.getElementById('left-team-players').textContent = data['ui-info']['ui-score']['teams'][0].players;
+    document.getElementById('right-team-players').textContent = data['ui-info']['ui-score']['teams'][1].players;
+    
+    document.getElementById('format-text').textContent = data['ui-info']['ui-score']['format'];
+
+    let minutes = data['ui-info']['ui-score']['timer'].minutes;
+    let seconds = data['ui-info']['ui-score']['timer'].seconds;
+    startTimer(minutes, seconds);
+      
+    updatePlayerText();
+  })
+  .catch(error => console.error('Error loading JSON:', error));
+
 document.addEventListener("DOMContentLoaded", function () {
+    updatePlayerText();
+});
+
+function updatePlayerText() {
     let players = document.querySelectorAll(".players-text");
 
     players.forEach(p => {
@@ -11,4 +37,30 @@ document.addEventListener("DOMContentLoaded", function () {
             lastSpan.remove();
         }
     });
-});
+}
+
+function startTimer(minutes, seconds) {
+    let timerElement = document.getElementById('timer-text');
+    let totalSeconds = minutes * 60 + seconds;
+
+    setInterval(function() {
+        let minutesDisplay = Math.floor(totalSeconds / 60);
+        let secondsDisplay = totalSeconds % 60;
+
+        if (secondsDisplay < 10) {
+            secondsDisplay = "0" + secondsDisplay;
+        }
+
+        if (minutesDisplay < 10) {
+            minutesDisplay = "0" + minutesDisplay;
+        }
+
+        timerElement.textContent = minutesDisplay + ":" + secondsDisplay;
+        if (totalSeconds > 0) {
+            totalSeconds--;
+        } else {
+            timerElement.textContent = "soon";
+            clearInterval();
+        }
+    }, 1000);
+}
